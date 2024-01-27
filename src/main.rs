37743d916664d4d0965c1c7f8289c2a1fbf406b7
Joshua Jolley <jolleyboy@gmail.com>
2024-01-27 12:02:24 -0700

    Read the logs directly from a child process instead of from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,42 @@
-use std::io::{self, Read};
+use std::io::{BufReader, BufRead};
 use std::{env, fs};
-use std::{thread, time};
+use std::process::{ Command, Stdio };
 
 fn main() {
-    let stdin = io::stdin();
-    let mut stdin = stdin.lock();
+    let mut cmd= Command::new("journalctl")
+        .arg("-b")
+        .arg("-u enshrouded.service")
+        .arg("-f")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
 
     let out_file = env::var("ENSHROUDED_PLAYER_COUNT_FILE")
         .unwrap_or_else(|_e| "./enshrouded-player-count.txt".to_string());
 
     let mut player_count = 0u8;
-    let mut buffer = String::new();
 
-    while let Ok(n_bytes) = stdin.read_to_string(&mut buffer) {
-        if n_bytes == 0 {
-            thread::sleep(time::Duration::from_millis(500));
-            break
+    let stdout = cmd.stdout.as_mut().unwrap();
+    let stdout_reader = BufReader::new(stdout);
+    let stdout_lines = stdout_reader.lines();
+
+
+    for result_line in stdout_lines {
+        match result_line {
+            Ok(line) => {
+                if line.contains("Remote player added") {
+                    player_count = player_count + 1;
+                    println!("Player added!  There are {} active players", player_count);
+                    fs::write(&out_file, player_count.to_string()).expect("Unable to write to file")
+                }
+
+                if line.contains("Player removed") {
+                    player_count = player_count - 1;
+                    println!("Player removed!  There are {} active players", player_count);
+                    fs::write(&out_file, player_count.to_string()).expect("Unable to write to file")
+                }
+            },
+            Err(_) => { /*do nothing*/ }
         }
-
-        for line in buffer.lines() {
-            if line.contains("Remote player added") {
-                player_count = player_count + 1;
-                println!("Player added!  There are {} active players", player_count);
-                fs::write(&out_file, player_count.to_string()).expect("Unable to write to file")
-            }
-
-            if line.contains("Player removed") {
-                player_count = player_count - 1;
-                println!("Player removed!  There are {} active players", player_count);
-                fs::write(&out_file, player_count.to_string()).expect("Unable to write to file")
-            }
-        }
-
-        buffer.clear();
     }
 }
